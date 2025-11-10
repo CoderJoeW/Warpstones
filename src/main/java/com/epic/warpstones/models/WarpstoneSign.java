f7a300073a4c9d5ea63ec6044cfcda6e949ee2ce
Joseph <jwilliamson@jslcodelabs.com>
2025-11-10 12:12:13 -0500

    Support for adding a list of lines directly without the sign

--- src/main/java/com/epic/warpstones/models/WarpstoneSign.java
+++ src/main/java/com/epic/warpstones/models/WarpstoneSign.java
@@ -1,8 +1,11 @@
 package com.epic.warpstones.models;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.block.Sign;
 import org.bukkit.block.sign.Side;
+
+import java.util.List;
 
 public class WarpstoneSign {
     public String title;
@@ -20,6 +23,16 @@ public class WarpstoneSign {
         }
     }
 
+    public WarpstoneSign(List<Component> lines) {
+        this.title = getContent(lines, 0);
+        this.name = getContent(lines, 1);
+
+        String idLine = getContent(lines, 2);
+        if (idLine.length() > 0) {
+            this.id = Integer.parseInt(idLine.split(" ")[1]);
+        }
+    }
+
     public boolean isValidWarpstoneSign() {
         if (this.title.equals("Warpstone")) {
             return true;
@@ -30,5 +43,9 @@ public class WarpstoneSign {
 
     private String getContent(Sign sign, int index) {
         return ((TextComponent)sign.getSide(Side.FRONT).lines().get(index)).content();
+    }
+
+    private String getContent(List<Component> lines, int index) {
+        return ((TextComponent)lines.get(index)).content();
     }
 }
