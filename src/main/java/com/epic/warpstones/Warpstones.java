c0efba6faa62d092275086a36884aa7edcef998c
Joseph <jwilliamson@jslcodelabs.com>
2025-11-12 15:57:01 -0500

    Update guide book to span multiple pages and describe operation in detail

--- src/main/java/com/epic/warpstones/Warpstones.java
+++ src/main/java/com/epic/warpstones/Warpstones.java
@@ -188,18 +188,48 @@ public class Warpstones extends JavaPlugin implements Listener {
         meta.setAuthor("Epic");
         meta.setLore(List.of("Instructions on how to create warpstones!"));
 
-        StringBuilder builder = new StringBuilder();
-        builder.append("Warpstone Guide Book: \n")
-                .append("Warpstones are created using special wording on signs\n")
-                .append("The first line of the warpstone must have \"Warpstone\"")
-                .append("The second line of the warpstone can be any name.")
-                .append("Keep in mind that this name is how you will link it to other warpstones\n")
-                .append("The third line of the sign will be the name of warpstone you wish to travel to.\n\n")
-                .append("Once a warpstone has been linked it will show green text \"Linked\" on the 4th row")
-                .append("Otherwise it will show red text \"Not Linked\"");
+        String page1 =
+                "§lWarpstone Guide§r\n" +
+                        "§8--------------------§r\n" +
+                        "\n" +
+                        "Create warpstones with signs.\n" +
+                        "Follow this format:\n" +
+                        "\n" +
+                        "§nLine 1§r: \"Warpstone\"\n" +
+                        "§nLine 2§r: §oYour Warpstone Name§r\n" +
+                        "§nLine 3§r: §oDestination Name§r\n" +
+                        "§nLine 4§r: Status (auto)\n";
 
-        meta.addPage(builder.toString());
+        String page2 =
+                "§lDetails§r\n" +
+                        "§8--------------§r\n" +
+                        "\n" +
+                        "Line 2 = this warpstone’s\n" +
+                        "name. Used by other signs\n" +
+                        "to link. Keep it unique.\n" +
+                        "\n" +
+                        "Line 3 = where to travel.\n" +
+                        "Must exactly match that\n" +
+                        "warpstone’s Line 2.\n" +
+                        "\n" +
+                        "Example:\n" +
+                        "  L2: §oTownSquare§r\n" +
+                        "  L3: §oMineEntrance§r\n";
 
+        String page3 =
+                "§lStatus§r\n" +
+                        "§8--------§r\n" +
+                        "\n" +
+                        "Line 4 shows:\n" +
+                        "  • §aLinked§r — names match\n" +
+                        "  • §cNot Linked§r — mismatch\n" +
+                        "\n" +
+                        "Quick checks:\n" +
+                        "• Spelling & case match\n" +
+                        "• No extra spaces\n" +
+                        "• Keep names short\n";
+
+        meta.addPage(page1, page2, page3);
         book.setItemMeta(meta);
 
         ShapedRecipe recipe = new ShapedRecipe(this.recipeKey, book);
