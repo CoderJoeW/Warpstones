3e8a94c0f9b8e0dc0a9caab39b93f24865554ed8
Joseph <jwilliamson@jslcodelabs.com>
2025-11-12 16:04:38 -0500

    remove reliance on id and replace with destination string

--- src/main/java/com/epic/warpstones/models/WarpstoneSign.java
+++ src/main/java/com/epic/warpstones/models/WarpstoneSign.java
@@ -10,27 +10,18 @@ import java.util.List;
 public class WarpstoneSign {
     public String title;
     public String name;
-    public int id;
+    public String destination;
 
     public WarpstoneSign(Sign sign) {
         this.title = getContent(sign, 0);
         this.name = getContent(sign, 1);
-
-        String idLine = getContent(sign, 2);
-
-        if (idLine.length() > 0) {
-            this.id = Integer.parseInt(idLine.split(" ")[1]);
-        }
+        this.destination = getContent(sign, 2);
     }
 
     public WarpstoneSign(List<Component> lines) {
         this.title = getContent(lines, 0);
         this.name = getContent(lines, 1);
-
-        String idLine = getContent(lines, 2);
-        if (idLine.length() > 0) {
-            this.id = Integer.parseInt(idLine.split(" ")[1]);
-        }
+        this.destination = getContent(lines, 2);
     }
 
     public boolean isValidWarpstoneSign() {
