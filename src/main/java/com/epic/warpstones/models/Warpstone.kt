b7fa8d257ee5a1f36e70711e40097eb1306c42b4
Joseph <jwilliamson@jslcodelabs.com>
2025-11-19 11:52:21 -0500

    Variables in model class need to be var not val

--- src/main/java/com/epic/warpstones/models/Warpstone.kt
+++ src/main/java/com/epic/warpstones/models/Warpstone.kt
@@ -3,11 +3,11 @@ package com.epic.warpstones.models
 import java.util.UUID
 
 class Warpstone {
-    public val x: Int = 0
-    public val y: Int = 0
-    public val z: Int = 0
+    public var x: Int = 0
+    public var y: Int = 0
+    public var z: Int = 0
 
-    public val name: String = ""
-    public val destination: String = ""
-    public val owner: UUID? = null
+    public var name: String = ""
+    public var destination: String = ""
+    public var owner: UUID? = null
 }
