3efaa52dd8ddf8682f9ab26c5790bae3101e6c5d
Joseph <jwilliamson@jslcodelabs.com>
2025-11-19 16:16:15 -0500

    implement kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
 group = "com.epic"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenCentral()
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
@@ -13,20 +15,29 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.9-rc1-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+// JVM toolchain
+kotlin {
+    jvmToolchain(21)
 }
 
-tasks.jar {
-    manifest {
-        attributes["paperweight-mappings-namespace"] = "mojang"
-    }
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveClassifier.set("")
+    mergeServiceFiles()
+
+    relocate("kotlin", "com.epic.warpstones.lib.kotlin")
+    relocate("kotlinx", "com.epic.warpstones.lib.kotlinx")
+
     destinationDirectory.set(layout.buildDirectory.dir("testing/plugins"))
 }
 
-kotlin {
-    jvmToolchain(24)
+tasks.build {
+    dependsOn(tasks.named("shadowJar"))
+}
+
+tasks.jar {
+    enabled = false
 }
